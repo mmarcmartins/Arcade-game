0df11e7bffbcd9033ae63ac792797f8c2cce6730
marcelo martins <mmarcmartins@gmail.com>
2018-12-20 11:09:18 -0200

    Jogo terminado
    Mudei CSS,HTML e o APP.JS para trazer uma maior individualidade ao projeto.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
 // Inimigos que nosso jogador deve evitar
 
 
-
-
-let vidas = [60,90,120];
+let res = "";
+document.onreadystatechange = () => {
+  if (document.readyState === 'complete') {
+     res = document.querySelector(".restart");     
+ }
+};
 
 class Character {
     constructor(sprite,col,row){
@@ -14,13 +17,6 @@ class Character {
 
     render(){        
       ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-      ctx.font =  "18px Arial"
-      ctx.textAlign = "center";
-      ctx.strokeWidth = 1;
-      ctx.fillText("Vidas: ",30,40);
-      for(const vida of vidas){
-          ctx.drawImage(Resources.get('images/Heart.png'),vida,13,30,40);
-      }
   }
 
   update(dt){
@@ -30,42 +26,59 @@ class Character {
 
 class Player extends Character{
 
-   constructor(sprite,col,row){
-        super(sprite,col,row,status);
-        this.status = status;
+ constructor(sprite,col,row){
+    super(sprite,col,row,status);
+    this.status = status;
+    this.vidas = [60,90,120];    
+}
 
-    }
+handleInput(bt){
 
-    handleInput(bt){
+    switch(bt){
+        case 'left' :                              
+        this.x-= (this.x <= 0 ) ? 0 : 101;
+        break;
 
-        switch(bt){
-            case 'left' :                              
-            this.x-= (this.x <= 0 ) ? 0 : 101;
-            break;
+        case 'right':                            
+        this.x+= (this.x >= 404 ) ? 0 : 101;              
+        break;
 
-            case 'right':                            
-            this.x+= (this.x >= 404 ) ? 0 : 101;              
-            break;
+        case 'up':                
+        this.y-= (this.y <= 70 ) ? -350 : 70;
+        this.x = (this.y == 350 ) ? 202: this.x;                                
+        break;
 
-            case 'up':                
-            this.y-= (this.y <= 70 ) ? -350 : 70;
-            this.x = (this.y == 350 ) ? 202: this.x;                                
-            break;
+        case 'down':
+        this.y += (this.y >= 420 ) ? 0 : 70;                          
+        break;
 
-            case 'down':
-            this.y += (this.y >= 420 ) ? 0 : 70;                          
-            break;
-        }
+        case 'enter':
 
-    }   
-
-
-    update(){
-        if(this.status === 0 ){            
+        if(this.status === 0)
            restartGame();
-           
-        }
+
+       break;
+   }
+
+}   
+
+render(){
+
+    super.render();
+    if(this.status === 0 ){
+        loseGame();            
     }
+    ctx.font =  "18px Arial"
+    ctx.textAlign = "center";
+    ctx.strokeWidth = 1;
+    ctx.fillText("Vidas: ",30,40);
+    for(const vida of this.vidas){
+      ctx.drawImage(Resources.get('images/Heart.png'),vida,13,30,40);
+  }
+}
+update(){
+
+}
 
 }
 
@@ -94,23 +107,34 @@ class Enemy extends Character {
 
 function checkCollisions(){
     for( const enemy of allEnemies){
-         if( enemy.x  >= ( player.x - 90 )  && enemy.x <= ( player.x + 90 )  && player.y === enemy.y ) {
-            vidas.splice(-1,1);
-              if(vidas.length > 0){
-                  player.x = 202;
-                  player.y = 350;                  
-              } else {                  
-                 player.status = 0;
-              }
-        }
-    }
+       if( enemy.x  >= ( player.x - 90 )  && enemy.x <= ( player.x + 90 )  && player.y === enemy.y ) {
+        player.vidas.splice(-1,1);
+        if(player.vidas.length > 0){
+          player.x = 202;
+          player.y = 350;                       
+      } else {                  
+       player.status = 0;
+       res.style.display = "block";
+   }
+}
+}
 
 }
 
-function restartGame(){        
-       for( const enemy of allEnemies){
-          enemy.speed = 0;
-       }
+function loseGame(){        
+    player.y = 350;
+    player.x = 202;
+    allEnemies.length = 0 ;
+    ctx.drawImage(Resources.get('images/gameover.png'),0,50,505,606);
+
+}
+
+function restartGame(){
+    player.vidas.length = 0;
+    player.vidas =  [60,90,120];
+    allEnemies = originalEnemiesPosition.slice(0);
+    player.status = 1;    
+    res.style.display = "none";
 }
 
 // Atualize a posição do inimigo, método exigido pelo jogo
@@ -139,12 +163,12 @@ let player = new Player('images/char-horn-girl.png',2,5,1);
 
 let allEnemies = [
 
-    new Enemy('images/enemy-bug.png', -1, 1, 9),
-    new Enemy('images/enemy-bug.png', -2, 2, 8),
-    new Enemy('images/enemy-bug.png', -3, 3, 15),
-    new Enemy('images/enemy-bug.png', -4, 1, 8),
-    new Enemy('images/enemy-bug.png', -5, 2, 10),
-    new Enemy('images/enemy-bug.png', -6, 3, 12),
+new Enemy('images/enemy-bug.png', -1, 1, 9),
+new Enemy('images/enemy-bug.png', -2, 2, 8),
+new Enemy('images/enemy-bug.png', -3, 3, 15),
+new Enemy('images/enemy-bug.png', -4, 1, 8),
+new Enemy('images/enemy-bug.png', -5, 2, 10),
+new Enemy('images/enemy-bug.png', -6, 3, 12),
 
 ];
 
@@ -157,7 +181,8 @@ document.addEventListener('keyup', function(e) {
         37: 'left',
         38: 'up',
         39: 'right',
-        40: 'down'
+        40: 'down',
+        13: 'enter'
     };
 
     player.handleInput(allowedKeys[e.keyCode]);
